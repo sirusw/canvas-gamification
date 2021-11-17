065ef96682d925c0a634ad2934fe7746b64e4295
sirusw <50982809+ourinsou@users.noreply.github.com>
2021-11-17 13:37:00 -0800

    Revert "Update"
    This reverts commit d16e899f71a4c8f40642d4aa87e06a0d9bc7d07c.

--- course/question-analysis/get_submission.py
+++ course/question-analysis/get_submission.py
@@ -1,7 +1,6 @@
 import requests
 import re
 from requests.auth import HTTPBasicAuth
-import math
 
 # TODO: Error handling:
 #  - input error
@@ -11,15 +10,10 @@ pw = 'wlsgss12184'
 java_submission_url = 'http://127.0.0.1:8000/api/java-submission/'
 parsons_submission_url = 'http://127.0.0.1:8000/api/parson-submission/'
 javaSub_list = []
-unary_op_list = ["-", "++", "--", "!", "~"]
-binary_op_list = ["+", "-", "*", "/", "%", "!=", "<", "<=", ">", ">=", "==", "^", "||", "|",
-                  "&&", "&", "+=", "-=", "*=", "/=", "%=", "&=", "^=", "="]
 
 
 class JavaSub:
-    def __init__(self, pk, uqj_id, q_id, u_id, ans_file, lines, blank_lines, comment_lines, import_lines, cc, method,
-                 operator, operand, operator_list, operand_list):
-        self.pk = pk
+    def __init__(self, uqj_id, q_id, u_id, ans_file, lines, blank_lines, comment_lines, import_lines, cc, method):
         self.uqj_id = uqj_id
         self.q_id = q_id
         self.u_id = u_id
@@ -30,11 +24,6 @@ class JavaSub:
         self.import_lines = import_lines
         self.cc = cc
         self.method = method
-        self.operator = operator
-        self.operand = operand
-        self.operator_list = operator_list
-        self.operand_list = operand_list
-
 
 def request_api(url, username, password):
     return requests.get(url, auth=HTTPBasicAuth(username, password))
@@ -105,75 +94,9 @@ def calc_cc(string):
 
 
 def num_method(string):
-    pattern = "(\n|\t| )(?!new|private|public)[\w]+\[?\]?(<?[\w]+?>?) +[\w]+ ?\((\w+ \w+\[\])?"
+    pattern = "(?!new|private|public)[\w]+\[?\]?(<?[\w]+?>?) +[\w]+ ?\((\w+ \w+\[\])?"
     num = len(re.findall(pattern, string))
     return num
-
-
-def num_op(string):
-    lines = string.split("\n")
-    operator = 0
-    operand = 0
-    operator_list = []
-    operand_list = []
-
-    print("////////////////////////////////////////")
-    for line in lines:
-        words = line.split(" ")
-        if "import" in line:
-            continue
-        if line.strip()[0:2] == '//':
-            continue
-        for i in range(len(words)):
-            for bi in binary_op_list:
-                if bi in words[i] and len(re.findall("^-[\w]+", words[i])) == 0:
-                    print("########")
-                    print(words)
-                    print("bi")
-                    print("########")
-
-                    if bi not in operator_list:
-                        operator_list.append(bi)
-                    if bi == words[i] and 0 < i < len(words) - 1:
-                        prev = words[i - 1]
-                        nxt = words[i + 1]
-                        if prev not in operand_list:
-                            operand_list.append(prev)
-                        if nxt not in operand_list:
-                            operand_list.append(nxt)
-                    else:
-                        split = words[i].split(bi)
-                        for op in split:
-                            if op not in operand_list:
-                                operand_list.append(op)
-
-                    operator += 1
-                    operand += 2
-                    break
-            for uni in unary_op_list:
-                pattern_exception = uni + "="
-
-                pattern_exception2 = "[^a-zA-Z\d\s]="
-                neg_sign = len(re.findall("^-[\w]+", words[i]))
-                rep = len(re.findall(pattern_exception2, words[i]))
-                if uni in words[i] and neg_sign > 0 and pattern_exception not in words[i] and rep == 0:
-                    print("########")
-                    print(words)
-                    print("########")
-
-                    if uni not in operator_list:
-                        operator_list.append(uni)
-                    split = words[i].split(uni)
-                    for op in split:
-                        if op != "" and op not in operand_list:
-                            operand_list.append(op)
-
-                    operator += 1
-                    operand += 1
-                    print(re.findall("^-[\w]+", words[i]))
-                    break
-
-    return [operator, operand, operator_list, operand_list]
 
 
 def code_analysis(sub_dict):
@@ -183,23 +106,15 @@ def code_analysis(sub_dict):
     import_lines = 0
     cc = 0
     method = 0
-    operator = 0
-    operand = 0
-    operator_list = []
-    operand_list = []
     for string in sub_dict.values():
         lines += num_lines(string)
         blank_lines += num_blank_lines(string)
         comment_lines += num_comment_lines(string)
         import_lines += num_import(string)
         cc += calc_cc(string)
-        method += num_method(string)
-        op_list = num_op(string)
-        operator += op_list[0]
-        operand += op_list[1]
-        operator_list.append(op_list[2])
-        operand_list.append(op_list[3])
-    return [lines, blank_lines, comment_lines, import_lines, cc, method, operator, operand, operator_list, operand_list]
+        method += num_method()
+
+    return [lines, blank_lines, comment_lines, import_lines, cc, method]
 
 
 def main():
@@ -212,44 +127,18 @@ def main():
 
     for i in range(len(java_sub)):
         ans_file = java_sub[i]['answer_files']
-        res = code_analysis(ans_file)
+        tmp = code_analysis(ans_file)
         uqj = java_sub[i]['uqj']
-        sub = JavaSub(java_sub[i]['pk'], uqj['id'], uqj['question_id'], uqj['user'], ans_file, res[0], res[1], res[2],
-                      res[3], res[4], res[5], res[6], res[7], res[8], res[9])
+        sub = JavaSub(uqj['id'], uqj['question_id'], uqj['user'], ans_file, tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5])
         javaSub_list.append(sub)
 
     for s in javaSub_list:
-        unique_operator = sum(len(y) for y in s.operator_list)
-        unique_operand = sum(len(x) for x in s.operand_list)
-        vocab = unique_operator + unique_operand
-        size = s.operator + s.operand
-        vol = size * math.log2(vocab)
-        difficulty = unique_operator / 2 + s.operand / unique_operand
-        effort = vol * difficulty
-        error = vol / 3000
-        test_time = effort / 18
-        print("PK: " + str(s.pk) + "\nUQJ id: " + str(s.uqj_id) + "\nQuestion id: " + str(s.q_id) + "\nUser id: " + str(
+        print("UQJ id: " + str(s.uqj_id) + "\nQuestion id: " + str(s.q_id) + "\nUser id: " + str(
             s.u_id) + "\nNumber of lines: " + str(s.lines)
               + "\nNumber of blank lines: " + str(s.blank_lines) + "\nNumber of comment lines: " + str(s.comment_lines)
               + "\nNumber of imports: " + str(s.import_lines) + "\nCyclomatic complexity: " + str(s.cc)
-              + "\nNumber of methods: " + str(s.method) + "\nNumber of operators: " + str(s.operator)
-              + "\nNumber of operands: " + str(s.operand)
-              + "\nNumber of unique operators: " + str(sum(len(y) for y in s.operator_list))
-              + "\nNumber of unique operands: " +  str(sum(len(x) for x in s.operand_list))
-              #+ "\nOperators: " + str(s.operator_list)
-              #+ "\nOperands: " + str(s.operand_list)
-              + "\nVocabulary: " + str(vocab)
-              + "\nSize: " + str(size)
-              + "\nVolume: " + str(round(vol, 2))
-              + "\nDifficulty: " + str(difficulty)
-              + "\nEffort: " + str(round(effort, 2))
-              + "\nError: " + str(round(error, 5))
-              + "\nTest time:" + str(round(test_time, 3)))
-        print("--------------------------")
-        for ans in s.ans_file.values():
-            print(ans)
-            print("--------------------------")
-        print("==========================================")
+              +  "\nNumber of methods: " + str(s.method))
+        print("==========================")
 
 
 if __name__ == "__main__":
